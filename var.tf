baede3779bb000cd6d3c76e3f399cd7e11ad79c2
Sedekie Kamara <sedekie05@gmail.com>
2023-01-14 13:13:03 -0600

    adding webserver infra, -12

---
+++ var.tf
@@ -0,0 +1,14 @@
+variable "ami_id" {
+    type = string
+    description = " ami ID"
+    default = "ami-0ae8d60635de460b2"
+}
+
+
+variable "instance_type" {
+    type = string
+    description = "Instance type"
+    default = "t3.micro"
+}
+
+  
